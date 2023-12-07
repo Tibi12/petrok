481305408dad3adc04cd113b1f8ee19645b6619b
Tibi12 <tibi.sunny@ssense.com>
2023-12-07 13:44:45 +0530

    change in url

--- src/components/product.js
+++ src/components/product.js
@@ -2,9 +2,12 @@
 import React from 'react';
 
 const Product = ({ product }) => {
+  const imageUrl = process.env.NODE_ENV === 'production'
+    ? `/${process.env.REACT_APP_REPOSITORY_NAME}/${product.imageUrl}`
+    : process.env.PUBLIC_URL + product.imageUrl;
   return (
     <div className="product">
-      <img src={process.env.PUBLIC_URL + product.imageUrl} alt={product.name} />
+      <img src={imageUrl} alt={product.name} />
       <h2>{product.name}</h2>
       <p>{product.description}</p>
     </div>
