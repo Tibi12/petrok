480429fdcc244f18d5dcc0ed1d298f3e553ba23d
Tibi12 <tibi.sunny@ssense.com>
2023-12-08 16:48:00 +0530

    added console statement

--- src/components/product.js
+++ src/components/product.js
@@ -5,6 +5,7 @@ const Product = ({ product }) => {
   const imageUrl = process.env.NODE_ENV === 'production'
     ? `/${process.env.REACT_APP_REPOSITORY_NAME}/${product.imageUrl}`
     : process.env.PUBLIC_URL + product.imageUrl;
+  console.log('Constructed imageUrl:', imageUrl);
   return (
     <div className="product">
       <img src={imageUrl} alt={product.name} />
